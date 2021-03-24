d408e11517989e4c4c43e73a49211494da0bd71d
Edgar Vilchis <81254443+evilch-1@users.noreply.github.com>
2021-03-24 10:49:16 -0600

    Uploading .js files

---
+++ Assignment_March24th/index.js
@@ -0,0 +1,28 @@
+// index.js
+/*
+
+Author: Edgar Vilchis
+Exploring new code concepts with some examples writing
+JavaScript Code
+
+Purpose: Use of constants, variables, functions, understanding
+the communication with HTML and threading
+
+*/
+
+var a = 'Hello world';
+
+//Normal function
+function b( ) {
+    return 'Bye!';
+}
+
+//Arrow function
+const b1 = ( ) => {
+    return 'Bye!!';
+}
+
+//Function Expression
+const b2 = function( ) {
+
+}
---
+++ Assignment_March24th/index1.js
@@ -0,0 +1,15 @@
+// index1.js
+/*
+
+Author: Edgar Vilchis
+Exploring code concepts with some examples writing
+JavaScript Code
+
+Purpose: Understanding how does the Google Chrome console
+receives information from JavaScript and calling it in
+the same place
+
+*/
+
+console.log(window.a);
+window.a = 'Bye world!'
