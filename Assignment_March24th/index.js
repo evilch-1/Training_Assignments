69592ea125334c7f7f4df2f1fcb6056ad52e9e4b
Edgar Vilchis <81254443+evilch-1@users.noreply.github.com>
2021-03-25 10:45:16 -0600

    Updating new code 03-25-21
    Adding aditional lines where it can be explained how we can see JS is dynamic and how typescrypt can be used to help us solve that "Dynamic issue".

--- Assignment_March24th/index.js
+++ Assignment_March24th/index.js
@@ -8,9 +8,14 @@ JavaScript Code
 Purpose: Use of constants, variables, functions, understanding
 the communication with HTML and threading
 
+Date of creation: March 24th, 2021
+Last modified: March 25th, 2021
+
 */
 
-var a = 'Hello world';
+//var a = 'Hello world';
+let a = 'Hello world';
+//Also we can use const
 
 //Normal function
 function b( ) {
@@ -23,6 +28,25 @@ const b1 = ( ) => {
 }
 
 //Function Expression
+//Function declaration
 const b2 = function( ) {
 
 }
+
+//Function invocation
+//b2( )
+
+//New code 03-25-21
+function add(num1, num2){//JS is dynamic
+    //Theres no way to use an explicit type like
+    //num1: number or int
+    return num1 + num2;
+}
+
+let result = add( 2,4 );
+//let result = add('3', 'steve');
+//Output: 3steve ---> no matter the type value
+console.log('Result: ', result);
+
+//To help developers with that last thing, the dynamic typing issue,
+//there exists typescript
