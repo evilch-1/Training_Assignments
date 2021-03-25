d3aea05f7fef89b44a2c2fa7ea16ca02a40198e5
Edgar Vilchis <81254443+evilch-1@users.noreply.github.com>
2021-03-25 11:24:04 -0600

    Adding code seen in training
    Added my own comments to the reference file.
    Here are some important notes about how does dynamic type works.
    
    Comment line 34 and save the file so it can be visible the change of a1 and b1 types.

--- 2021-03-25/reference.js
+++ 2021-03-25/reference.js
@@ -1,1 +1,40 @@
+/*
+
+Author: Edgar Vilchis
+Exploring new code concepts with some examples writing
+JavaScript Code (reference types)
+
+Purpose: Primitive and reference types exploration
+Important concepts about memory and how it is passed to memory
+a dynamic type and how is it changing while mutating properties
+are accesing to that type.
+
+Date of creation: March 25th, 2021
+Last modified: March 25th, 2021
+
+*/
+
 //primitive type
+let a = 3;
+let b;
+
+b = a; //b = 3
+a = 2; // a = 2
+
+console.log(a);
+console.log(b);
+
+//reference type
+let a1 = { greeting: 'Hi' };
+let b1;
+
+b1 = a1; //b1 = { greeting: 'Hi'}
+
+//Changing the greeting to Hello:
+a1.greeting = 'Hello'; //a1 = { greeting: 'Hello'}
+//Very important:
+//NOW, BOTH a1 and b1 are pointing to Hello because of the dynamic type
+
+
+console.log(a1);
+console.log(b1);
